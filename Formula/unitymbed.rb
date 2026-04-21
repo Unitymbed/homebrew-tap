010a639c898e19992f7514d74cf3097caa3e69f1
JediJade <87669729+JediJade@users.noreply.github.com>
2026-04-22 03:28:42 +0700

    v0.1.8: free signup command

--- Formula/unitymbed.rb
+++ Formula/unitymbed.rb
@@ -1,25 +1,25 @@
 class Unitymbed < Formula
   desc "On-premise AI firmware assistant for ARM Cortex MCUs"
   homepage "https://github.com/Unitymbed/homebrew-tap"
-  version "0.1.7"
+  version "0.1.8"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Unitymbed/homebrew-tap/releases/download/v0.1.7/unitymbed-mac-arm64"
-      sha256 "b375dc3f9002098596e6c9bfea977315bed6d5bd94d7e4cf831370586bb22193"
+      url "https://github.com/Unitymbed/homebrew-tap/releases/download/v0.1.8/unitymbed-mac-arm64"
+      sha256 "554a79b23cb8930664b59a93d1c0980cb693148b262c5059b30e590547beee70"
     else
-      url "https://github.com/Unitymbed/homebrew-tap/releases/download/v0.1.7/unitymbed-mac-x64"
-      sha256 "7e92cb3c608015534c610b1a7d034bfffc92b9249cdd45b9b5eae07102221b6d"
+      url "https://github.com/Unitymbed/homebrew-tap/releases/download/v0.1.8/unitymbed-mac-x64"
+      sha256 "e903307e27ef4fb4a54daa9e784b6e7fdff789544209961246dae8d576127e04"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Unitymbed/homebrew-tap/releases/download/v0.1.7/unitymbed-linux-arm64"
-      sha256 "6a33c94b1f1f8c427ea7ce8f2b04f2e804ccb88c3bd4599a2573f03e06556846"
+      url "https://github.com/Unitymbed/homebrew-tap/releases/download/v0.1.8/unitymbed-linux-arm64"
+      sha256 "ffec3a848bae266755a4549a96f7c44a717954ca61c169abc2e51f098ecd18cb"
     else
-      url "https://github.com/Unitymbed/homebrew-tap/releases/download/v0.1.7/unitymbed-linux-x64"
-      sha256 "9f1a5f0516624caac7a92efd9955859bab37a2d06f4205edbf1165bbe2361881"
+      url "https://github.com/Unitymbed/homebrew-tap/releases/download/v0.1.8/unitymbed-linux-x64"
+      sha256 "eb6c69ad4ac2a7bfc5c272813479b85b18270699a27c5e69fc4bf98fff8ecc19"
     end
   end
 
